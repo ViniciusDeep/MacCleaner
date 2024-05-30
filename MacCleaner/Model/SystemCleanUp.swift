9efdaee9c0db349472fee44c8ed25fe9b938cb27
Vinicius.Correia1 <viniciuscorreia11@gmail.com>
2024-05-30 00:46:12 -0300

    feature: adding function to find files

---
+++ MacCleaner/Model/SystemCleanUp.swift
@@ -0,0 +1,8 @@
+//
+//  SystemCleanUp.swift
+//  MacCleaner
+//
+//  Created by Vinicius Mangueira on 29/05/24.
+//
+
+import Foundation
---
+++ MacCleaner/Model/SystemMonitorHelper.swift
@@ -0,0 +1,8 @@
+//
+//  SystemMonitorHelper.swift
+//  MacCleaner
+//
+//  Created by Vinicius Mangueira on 30/05/24.
+//
+
+import Foundation
