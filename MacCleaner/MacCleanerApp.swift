a9d721d3d3219304eb1e4110b19d7f71aee3bb9c
Vinicius.Correia1 <vinicius.correia@wendys.com>
2024-05-29 23:38:06 -0300

    Initial Commit

---
+++ MacCleaner/MacCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacCleanerApp.swift
+//  MacCleaner
+//
+//  Created by Vinicius Mangueira on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MacCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
